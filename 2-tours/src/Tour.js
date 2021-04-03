09a09d9b813ab86bf5b32c386c90c0bfeb60ba94
Olha Salatova <olga.sharpmind@gmail.com>
2021-04-03 12:20:05 +0200

    Dynamic tours list, with the option to remove the tour from list

---
+++ 2-tours/src/Tour.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">{price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 300)}...`}
+          <button
+            onClick={() => {
+              setReadMore(!readMore);
+            }}
+          >
+            {readMore ? "Show less" : "Read More"}
+          </button>
+        </p>
+        <button id={id} className="delete-btn" onClick={() => removeTour(id)}>
+          not interested
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default Tour;
